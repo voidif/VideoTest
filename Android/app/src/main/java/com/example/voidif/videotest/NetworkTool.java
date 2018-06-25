5d5c45699dae9ef6f05c261360a06a413b398414
voidif <529399971@qq.com>
2018-06-25 17:23:39 +0800

    tcp transmission

--- Android/app/src/main/java/com/example/voidif/videotest/NetworkTool.java
+++ Android/app/src/main/java/com/example/voidif/videotest/NetworkTool.java
@@ -1,6 +1,9 @@
 package com.example.voidif.videotest;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.net.*;
 
 public class NetworkTool {
@@ -8,27 +11,53 @@ public class NetworkTool {
     private static final String ipName = "192.168.0.105";
 
     //发送视频流到PC端,这里传递过来的参数os中保存的是视频输出流数据
-    public static void send(ByteArrayOutputStream os) {
-        byte[] data = os.toByteArray();
+    public static void send(ByteArrayOutputStream data) {
+        //1.创建客户端Socket，指定服务器地址和端口
 
-        /*
-         * 向服务器端发送数据
-         */
-
-        InetAddress address = null;
+        byte[] buffer = new byte[1024];
         try {
-            // 1.定义服务器的地址、端口号、数据
-            address = InetAddress.getByName(ipName);
-            int port = 30000;
-            // 2.创建数据报，包含发送的数据信息
-            DatagramPacket packet = new DatagramPacket(data, data.length, address, port);
-            // 3.创建DatagramSocket对象
-            DatagramSocket socket = new DatagramSocket();
-            // 4.向服务器端发送数据报
-            socket.send(packet);
-        } catch (Exception e) {
+            Socket socket = null;
+            socket = new Socket(ipName, 30000);
+            //2.获取输出流，向服务器端发送信息
+            OutputStream os = socket.getOutputStream();//字节输出流
+            //实例化内存输入流，将视频流数据写入到内存中
+            ByteArrayInputStream inputFromOs = new ByteArrayInputStream(data.toByteArray());
+            //不断从内存中读取数据到buffer中，不断再从buffer中将视频数据发送到outSocket流中
+            int amount;
+            while((amount = inputFromOs.read(buffer)) != -1)
+                os.write(buffer, 0, amount);
+
+            //close
+            data.flush();
+            inputFromOs.close();
+            data.close();
+            os.close();
+            socket.shutdownOutput();//关闭输出流
+            socket.close();
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
     }
 }
+
+//        byte[] data = os.toByteArray();
+//
+//        /*
+//         * 向服务器端发送数据(UDP)
+//         */
+//
+//        InetAddress address = null;
+//        try {
+//            // 1.定义服务器的地址、端口号、数据
+//            address = InetAddress.getByName(ipName);
+//            int port = 30000;
+//            // 2.创建数据报，包含发送的数据信息
+//            DatagramPacket packet = new DatagramPacket(data, data.length, address, port);
+//            // 3.创建DatagramSocket对象
+//            DatagramSocket socket = new DatagramSocket();
+//            // 4.向服务器端发送数据报
+//            socket.send(packet);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
--- Receiver/src/Main.java
+++ Receiver/src/Main.java
@@ -18,56 +18,39 @@ public class Main {
 
 
         try{
-            /*
-             * 接收客户端发送的数据
-             */
-            // 1.创建服务器端DatagramSocket，指定端口
-            DatagramSocket socket = new DatagramSocket(30000);
-            // 2.创建数据报，用于接收客户端发送的数据
-            byte[] data = new byte[1024];// 创建字节数组，指定接收的数据包的大小
-            DatagramPacket packet = new DatagramPacket(data, data.length);
-            // 3.接收客户端发送的数据
-            System.out.println("****服务器端已经启动，等待客户端发送数据");
-            socket.receive(packet);// 此方法在接收到数据报之前会一直阻塞
-            // 4.读取数据
-            String info = new String(data, 0, packet.getLength());
-
-
-
-//            DataInputStream dis;
-//
-//            ServerSocket server=new ServerSocket(30000);// listening for connections on port 8000
-//
-//            Socket socket=server.accept();// accept and connect to a scocket
+            ServerSocket server = new ServerSocket(30000);// listening for connections on port 8000
+            Socket childSocket = server.accept();// accept and connect to a scocket
 
 
             while(true){
-//                dis=new DataInputStream(socket.getInputStream()); 	// create an input stream
-//
-//                int len=dis.readInt();// read the length of the byte array sent by the client transmitting video
-//
-//
-//                byte [] buffer=new byte[len]; // initialize a byte array of the length received from the client
-//
-//                dis.readFully(buffer,0,len);  // read the byte array data sent from the client and write it into the buffer array
-//
+                //3.连接后获取输入流，读取客户端信息
+                InputStream is = childSocket.getInputStream();     //获取输入流
+                byte[] data = is.readAllBytes();
 
-                InputStream in = new ByteArrayInputStream(data, 0, packet.getLength());
-
-                BufferedImage im=ImageIO.read(in); // creating an image from the byte array
-
+                InputStream in = new ByteArrayInputStream(data);
+                BufferedImage im = ImageIO.read(in); // creating an image from the byte array
                 jlb.setIcon(new ImageIcon(im));// setting the image onto the JLabel container
-
                 jfr.add(jlb);// JLabel container being added to the JFrame container
-
                 jfr.pack();
-
                 jfr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
                 jfr.setVisible(true);
-
                 System.gc();
             }
         }
         catch(Exception e){e.printStackTrace();}
     }}
+
+
+//            /*
+//             * 接收客户端发送的数据(UDP)
+//             */
+//            // 1.创建服务器端DatagramSocket，指定端口
+//            DatagramSocket socket = new DatagramSocket(30000);
+//            // 2.创建数据报，用于接收客户端发送的数据
+//            byte[] data = new byte[1024];// 创建字节数组，指定接收的数据包的大小
+//            DatagramPacket packet = new DatagramPacket(data, data.length);
+//            // 3.接收客户端发送的数据
+//            System.out.println("****服务器端已经启动，等待客户端发送数据");
+//            socket.receive(packet);// 此方法在接收到数据报之前会一直阻塞
+//            // 4.读取数据
+//            String info = new String(data, 0, packet.getLength());
